70cd5b157d20cecb9e54bb9bba76368a900c65a6
JMSONNE <jordynmsonne@gmail.com>
2024-07-29 16:35:17 -0500

    fixed profile picture import in home

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
-import profilePic from '../assets/profile_pic.png';
 import '../src/styles.css'
+import 
 
 const Header = () => {
     return (
