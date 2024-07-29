7df74f29f601a12dbff7861954eb081feb623686
JMSONNE <jordynmsonne@gmail.com>
2024-07-29 16:47:47 -0500

    added browser router to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../src/styles.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
   ,
 )
