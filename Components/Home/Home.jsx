4db4b99bf757c8ef5ddc1349a4a1ec8bb4cb9026
JMSONNE <jordynmsonne@gmail.com>
2024-07-29 19:09:58 -0500

    formatted home page

--- Components/Home/Home.jsx
+++ Components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import '../../src/styles.css'
 import profilePic from '../../assets/profile_pic.png'
+import logo from '../../assets/JSCC.png'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 const Home = () => {
 
@@ -33,33 +35,40 @@ const Home = () => {
                 </header>
             </div>
 
-            {/* Section for external projects to be displayed */}
 
+            {/* Section for main content */}
             <main className="body">
 
-                {/* Card for JavaScripts Cofee */}
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        sx={{ height: 140 }}
-                        image="/static/images/cards/contemplative-reptile.jpg"
-                        title="JavaScripts Coffee"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            JavaScripts Coffee Co.
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            My capstone project from my coding bootcamp at Full Stack Academy! The client side is built with
-                            React and uses components from Material UI. The backend is built using Express.js and a PostgreSQL
-                            database.
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small" onClick={handleJavaScriptsLink}>Visit</Button>
-                    </CardActions>
-                </Card>
+                {/* Section for external projects to be displayed using cards from MUI */}
+                <Grid container spacing={3} sx={{ m: 3, display: 'flex', justifyContent: 'center', textAlign: 'center', p: 3 }}>
 
-            </main>
+                    {/* Card for JavaScripts Cofee arranged using the Grid component from MUI*/}
+
+                    <Grid item xs={6}>
+                        <Card className='card' sx={{ maxWidth: 400, minWidth: 300, maxHeight: 370 }}>
+                            <CardMedia
+                                sx={{ height: 140 }}
+                                image={logo}
+                                title="JavaScripts Coffee"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    JavaScripts Coffee Co.
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    My capstone project from my coding bootcamp at Full Stack Academy! The client side is built with
+                                    React and uses components from Material UI. The backend is built using Express.js and a PostgreSQL
+                                    database.
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" onClick={handleJavaScriptsLink}>Visit</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                </Grid>
+
+            </main >
         </>
     );
 }
