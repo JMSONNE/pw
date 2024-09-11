364c30d21a4701eda32000cf9bcc610c9c924c33
JMSONNE <jordynmsonne@gmail.com>
2024-09-11 18:41:19 -0500

    changed avatar to take up more of the screen on smaller devices

--- Components/Home/Home.jsx
+++ Components/Home/Home.jsx
@@ -30,8 +30,7 @@ const Home = () => {
                     <Avatar
                         className='profile-pic'
                         alt="Jordyn Sonne"
-                        src={profilePic}
-                        sx={{ width: 225, height: 225, margin: 5, zIndex: 1 }} />
+                        src={profilePic} />
                     <h1 className="quote">"Machines take me by surprise with great frequency." -Alan Turing</h1>
                 </header>
             </div>
