1910f48992ba69896c360703a061a0c898fc15dd
JMSONNE <jordynmsonne@gmail.com>
2024-09-15 00:07:50 -0500

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import '../src/styles.css'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Footer } from "../Components/Footer/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const darkTheme = createTheme({
   palette: {
@@ -18,6 +19,7 @@ function App() {
   return (
     <>
       <ThemeProvider theme={darkTheme}>
+        <Analytics />
         <CssBaseline />
         <Router />
         <Footer />
